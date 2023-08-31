76a6d550dce1f8e53a0a715ca569bbfd6b9158df
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-08-31 11:36:35 +0200

    configures connection to mongoDB using java

---
+++ backEnd/movies/src/test/java/pawel_obrzut/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package pawel_obrzut.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
